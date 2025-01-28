587035209187996589c03f84a0298bdc2bd5ca03
DavidePanza <davide.panza@gmail.com>
2025-01-28 13:18:36 +0100

    Save changes to dev.py before merge

--- dev.py
+++ dev.py
@@ -32,6 +32,10 @@ def upload_files() -> pd.DataFrame:
 
     # Check if the user uploaded a file
     if uploaded_file is not None:
+        # Reset session state to clear previous transformations
+        if "df_clean" in st.session_state:
+            del st.session_state.df_clean  # Clear the cleaned DataFrame
+
         # Read the CSV file into a DataFrame
         try:
             df = pd.read_csv(uploaded_file)
@@ -53,12 +57,9 @@ def return_feat(df: pd.DataFrame) -> list:
 
 
 def select_vars(numeric_feat, categorical_feat):
-
     # Store selected states
-    if "selected_numeric" not in st.session_state:
-        st.session_state.selected_numeric = []
-    if "selected_categorical" not in st.session_state:
-        st.session_state.selected_categorical = []
+    selected_numeric = []
+    selected_categorical = []
 
     # Create two columns
     col1, col2 = st.columns(2)
@@ -68,58 +69,91 @@ def select_vars(numeric_feat, categorical_feat):
         st.subheader("Numerical Variables")
         for var in numeric_feat:
             # Checkbox for each variable
-            selected = st.checkbox(var, value=(var in st.session_state.selected_numeric))
-            if selected and var not in st.session_state.selected_numeric:
-                st.session_state.selected_numeric.append(var)
-            elif not selected and var in st.session_state.selected_numeric:
-                st.session_state.selected_numeric.remove(var)
+            selected = st.checkbox(var, value=(var in selected_numeric))
+            if selected and var not in selected_numeric:
+                selected_numeric.append(var)
+            elif not selected and var in selected_numeric:
+                selected_numeric.remove(var)
 
     with col2:
         st.subheader("Categorical Variables")
         for var in categorical_feat:
             # Checkbox for each variable
-            selected = st.checkbox(var, value=(var in st.session_state.selected_categorical))
-            if selected and var not in st.session_state.selected_categorical:
-                st.session_state.selected_categorical.append(var)
-            elif not selected and var in st.session_state.selected_categorical:
-                st.session_state.selected_categorical.remove(var)
+            selected = st.checkbox(var, value=(var in selected_categorical))
+            if selected and var not in selected_categorical:
+                selected_categorical.append(var)
+            elif not selected and var in selected_categorical:
+                selected_categorical.remove(var)
     
-    st.write("Selected Items:", st.session_state.selected_numeric)
-    st.write("Selected Items:", st.session_state.selected_categorical)
+    st.write("Selected Items:", selected_numeric)
+    st.write("Selected Items:", selected_categorical)
+
+    return selected_numeric, selected_categorical
 
 
-def process_na(df: pd.DataFrame, treshold=.20) -> pd.DataFrame:
-
+def process_missing_values(df: pd.DataFrame, selected_numeric, selected_categorical, treshold = .2) -> pd.DataFrame:
     # drop columns with missing values
     cols_to_drop = []
+    df_selected = df[selected_numeric + selected_categorical]
+    print(df_selected)
 
-    for col in df.columns: 
-        len_df = df.shape[0]
-        if sum(df[col].isnull()) > 0:
-            tot_null = sum(df[col].isnull())
-            avg_null = tot_null/len_df
+    for col in df_selected.columns: 
+        len_df = df_selected.shape[0]
+        if sum(df_selected[col].isnull()) > 0:
+            tot_null = sum(df_selected[col].isnull())
+            avg_null = (tot_null/len_df) * 100
             st.write(f'{col:20} % null values: {avg_null}%')
             if avg_null > treshold:
                 cols_to_drop.append(col)
 
     st.write(cols_to_drop)
-    # df = df.drop(columns=cols_to_drop)
-    # df = df.drop(['Id'], axis=1)
+
+    return df.drop(columns=cols_to_drop), cols_to_drop
+
+
+def process_outliers(df: pd.DataFrame, treshold: int = 2) -> pd.DataFrame:
+    num_cols = df.select_dtypes(include='number')
+    df_z = num_cols.apply(zscore)
+    outliers = abs(df_z) > treshold
+    row_to_remove = np.where(outliers.any(axis=1))[0]
+    df = df.drop(row_to_remove)
+
+    return df
 
 
 def main() -> None:
     configure_page()
     configure_overview()
     separation()
+
     df = upload_files()
     separation()
-    if df is not None:
-        numeric_feat, categorical_feat = return_feat(df)
-        select_vars(numeric_feat, categorical_feat)
-        separation()
-        process_na(df)
-    
 
+    if df is not None:
+        if "df_clean" not in st.session_state:
+            st.session_state.df_clean = df
+
+        numeric_feat, categorical_feat = return_feat(st.session_state.df_clean)
+        st.markdown("# Features Selection")
+        selected_numeric, selected_categorical = select_vars(numeric_feat, categorical_feat)
+        separation()
+
+        st.markdown("# Data Preprocessing")
+        st.markdown("<br><h4>1 Drop Columns with Missing Values</h3>", unsafe_allow_html=True)
+        na_threshold = st.slider("Select threshold of missing values to drop variables", 0, 100, 20)
+        st.markdown("<br><h4>2 Remove Outliers</h3>", unsafe_allow_html=True)
+        outliers_threshold = st.slider("Select z-threshold to drop outliers", 0, 6, 3)
+
+        # Button to trigger column drop
+        if st.button("Apply Preprocessing"):
+            rows_before = st.session_state.df_clean.shape[0]
+            st.session_state.df_clean, cols_to_drop = process_missing_values(
+                st.session_state.df_clean, selected_numeric, selected_categorical, na_threshold)
+            st.session_state.df_clean = process_outliers(st.session_state.df_clean, outliers_threshold)
+            st.session_state.df_clean = st.session_state.df_clean.drop_duplicates()
+            st.write(f"Remaining columns after drop: {st.session_state.df_clean.columns}")
+            st.write(f"rows before: {rows_before}, rows after: {st.session_state.df_clean.shape[0]}")
+    
 
 if __name__ == "__main__":
     main()
